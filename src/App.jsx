4b6fddaf5c71c337633435989c596f011a443ae3
wamancera <williammancera2@gmail.com>
2023-03-07 22:46:55 -0500

    Creando components Cards

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+import { Form } from './Components/Form/Form';
+import { Card } from './Components/Card/Card';
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Carga de estudiasnte</h1>
+      <Form/>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
